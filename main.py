a0a58a49a408030cbe9940c25dc7cc73b1f9f35c
DarkD0 <105650020+DarkD0@users.noreply.github.com>
2022-07-24 12:54:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,48 +3,29 @@ import requests
 import os
 import random
 import time
-import base64
-token = "5545279600:AAG536spuk4bFBaeLUT96WlpPo9BMHyz5y0"
+token = "5400141055:AAGwQDwGR9ojA0qgie_ruTJ9nE2Ghfkole8"
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id,f"<strong>Hi {message.from_user.first_name},\n=== === ===\nWellcome! Send Number & Password To Activate \n Exmple = number:password </strong>",parse_mode="html")
+    bot.send_message(message.chat.id,f"<strong>Hi {message.from_user.first_name},\n=== === ===\nWellcome! Send Dark To Active </strong>",parse_mode="html")
 @bot.message_handler(func=lambda m:True)
 def ps(message):
     msg = message.text
-    number = msg.split(':')[0]
-    password = msg.split(':')[1]
-    bot.send_message(message.chat.id,f"<strong>Done Recived.. Wait! </strong>",parse_mode="html")
-    message =number+":"+password
-    message_bytes = message.encode('ascii')
-    base64_bytes = base64.b64encode(message_bytes)
-    base64_message = base64_bytes.decode('ascii')
-    d="Basic"+" "+base64_message
-    url='https://mab.etisalat.com.eg:11003/Saytar/rest/authentication/loginWithPlan'
-    hd={"applicationVersion": "2",
-              "applicationName": "MAB",
-              "Accept": "text/xml",
-              "applicationPassword": "ZFZyqUpqeO9TMhXg4R/9qs0Igwg\u003d",
-              "Authorization":d,
-              "APP-BuildNumber": "547",
-              "APP-Version": "22.13.0",
-              "OS-Type": "Android",
-              "OS-Version": "8",
-              "APP-STORE": "Huawei",
-              "Is-Corporate": "false",
-              "Content-Type": "text/xml; charset\u003dUTF-8",
-              "Content-Length": "290",
-              "Host": "mab.etisalat.com.eg:11003",
-              "Connection": "Keep-Alive",
-              "Accept-Encoding": "gzip",
-              "User-Agent": "okhttp/3.12.8",
-              "ADRUM_1": "isMobile:true",
-              "ADRUM": "isAjax:true"}
-    data="<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><loginRequest><deviceId></deviceId><firstLoginAttempt>true</firstLoginAttempt><modelType>J19SG</modelType><osVersion></osVersion><platform>Android</platform><udid></udid></loginRequest>"
-    r=requests.post(url, headers=hd, data=data)
-    coki = r.headers["Set-Cookie"]
-    token = coki[:-127]
-    bot.send_message(message.chat.id,f"<strong>Done Login : {token}</strong>",parse_mode="html")
+    if msg == "Dark":
+        bot.send_message(message.chat.id,f"<strong>Done Recived.. Wait! </strong>",parse_mode="html")
+        n = '0123456789'
+        l = 'abcdefghijklmnopqrstuvwxyz'
+        nam = ['ahmed','mohamed','wael','essam','moka','ismail','mahmoud','anas','salah']
+        name = random.choice(nam)
+        number = '01'+random.choice('0125')+random.choice(n)+random.choice(n)+random.choice(n)+random.choice(n)+random.choice(n)+random.choice(n)+random.choice(n)+random.choice(n)
+        email = random.choice(l)+random.choice(l)+random.choice(l)+random.choice(l)+random.choice(l)+random.choice(l)+random.choice(l)
+#         print(number)
+#         print(email)
+        url = "https://api.rida-egypt.xyz/api/user/register"
+        hd = {"Host":"api.rida-egypt.xyz"}
+        data = {"phone":number,"password":number,"email":email+"@gmail.com","full_name":name}
+        r = requests.post(url,headers=hd,data=data).json()
+        bot.send_message(message.chat.id,f"<strong> Done Create Account : {number} \n Password : {password}</strong>",parse_mode="html")
 bot.polling()
